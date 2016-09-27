67eb6f218542ea3db9dcfafd2fa8d6db9959d5fa
WilliamIV <william.green.iv@gmail.com>
2016-09-27 19:49:45 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPracticeProb5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Give me a number to double.");
+            int userNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(Double(userNum));
+        }
+
+    static int Sum(int[] example)
+        {
+            int total = 0;
+            foreach (int value in example)
+         {
+            total += value;
+         }
+     return total;
+    }
+
+    static int Double(int userNum)
+        {
+            int doubleMint = userNum * 2;
+            return doubleMint;
+        }
+
+    }
+}
